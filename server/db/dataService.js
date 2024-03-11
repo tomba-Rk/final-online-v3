f0d532651784f14fc39c26ac69041799d3ca7132
tomba-Rk <aarkedesign@gmail.com>
2024-03-11 20:32:09 +0630

    test

--- server/db/dataService.js
+++ server/db/dataService.js
@@ -1,4 +1,5 @@
 const db = require('../config/firebase');
+const admin = require('firebase-admin');
 
 async function addData(data) {
     try {
@@ -10,6 +11,19 @@ async function addData(data) {
                                 expirationTime: expirationTimestamp.toISOString() // Storing as ISO string
                             });
         console.log("Data added:", res);
+
+    
+
+    // Reference to the 'today-test' document in the 'RandomNumber' collection
+    const docRef = db.collection('RandomNumber').doc('today-test');
+    
+    // Update the document with the random number, adding it to the 'numbers' array field
+    // This will create the document if it doesn't exist, or update it without overwriting other content
+    const res_new = await docRef.set({
+        numbers: admin.firestore.FieldValue.arrayUnion(data)
+    }, { merge: true }); // Using 'merge: true' to ensure other fields are not overwritten
+    
+    console.log("Random number added:", res_new);
     } catch (error) {
         console.error("Error adding data:", error);
         // Handle the error appropriately
